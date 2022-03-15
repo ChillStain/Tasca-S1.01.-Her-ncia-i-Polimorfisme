dee373425e92c588009e484b0622a0f7dec0bbb2
ChillStain <101631488+ChillStain@users.noreply.github.com>
2022-03-15 13:07:37 +0100

    Add files via upload

---
+++ N1Ex1CG/src/N1Ex1/Vehicle.java
@@ -0,0 +1,17 @@
+package N1Ex1;
+
+public class Vehicle {
+	public Vehicle() {
+		
+	}
+	public void iniciar(){
+		System.out.println("iniciar");
+		
+		
+	}
+	public static void main(String[] args) {
+		new Vehicle().iniciar();
+		
+	}
+
+}

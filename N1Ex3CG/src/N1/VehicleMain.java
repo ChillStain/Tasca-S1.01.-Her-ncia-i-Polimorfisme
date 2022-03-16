f272e23a63c1e8021adb833c36fe13d78c2d81fb
ChillStain <101631488+ChillStain@users.noreply.github.com>
2022-03-16 12:07:59 +0100

    Add files via upload

---
+++ N1Ex3CG/src/N1/VehicleMain.java
@@ -0,0 +1,23 @@
+package N1;
+
+public class VehicleMain {
+
+
+	public static void main(String[] args) {
+		
+		
+		
+		new Vehicle().start();
+		new Vehicle().stop();
+		new Vehicle().speedUp();
+		
+		
+		
+		
+	
+	
+		
+		
+	}
+}
+

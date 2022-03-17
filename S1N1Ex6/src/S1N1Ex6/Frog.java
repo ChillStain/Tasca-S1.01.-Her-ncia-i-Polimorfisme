920a5a301e95b5599d348ea964a1d7fd442804b1
ChillStain <101631488+ChillStain@users.noreply.github.com>
2022-03-17 13:16:21 +0100

    Add files via upload

---
+++ S1N1Ex6/src/S1N1Ex6/Frog.java
@@ -0,0 +1,12 @@
+package S1N1Ex6;
+
+import S1N1Ex6.Amphibian;
+
+public class Frog extends Amphibian {
+    public Frog() {
+    }
+
+    public void croac() {
+        System.out.println("Croackkkkkk¡¡¡¡¡");
+    }
+}
---
+++ S1N1Ex6/src/S1N1Ex6/Main.java
@@ -0,0 +1,16 @@
+package S1N1Ex6;
+
+public class Main {
+    public Main(){
+
+    }
+
+    public static void main(String[] args) {
+        Frog rana = new Frog();
+        rana.eat();
+        rana.move();
+        rana.breath();
+        rana.croac();
+        
+    }
+}

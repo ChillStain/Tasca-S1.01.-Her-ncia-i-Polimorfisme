38847522e8482560e0c273d30f2fbd98b9e7d609
ChillStain <101631488+ChillStain@users.noreply.github.com>
2022-03-21 10:05:41 +0100

    Add files via upload

---
+++ S1N3Ex1/Base.java
@@ -0,0 +1,12 @@
+package S1N3Ex1;
+
+public class Base {
+
+    public void method() {
+        System.out.println("Method 1");
+    }
+    public void method2(){
+        System.out.println("Method 2");
+    }
+
+}
---
+++ S1N3Ex1/Derivada.java
@@ -0,0 +1,9 @@
+package S1N3Ex1;
+
+public class Derivada extends Base {
+
+    @Override
+    public void method2() {
+        System.out.println("Substitute");
+    }
+}
